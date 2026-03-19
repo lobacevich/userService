849a36f8e8de3d57b0ab1bca32ffc98162a27088
Ihar Labatsecich <lobacevich_@tut.by>
2026-03-19 09:05:05 +0300

    fix/add cascade attribute to User entity

--- src/main/java/by/lobacevich/entity/User.java
+++ src/main/java/by/lobacevich/entity/User.java
@@ -1,5 +1,6 @@
 package by.lobacevich.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -43,6 +44,6 @@ public class User extends AppEntity {
     @Column(nullable = false, unique = true, length = 63)
     private String email;
 
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, orphanRemoval = true)
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     List<PaymentCard> paymentCards = new ArrayList<>();
 }
